2ab59d19ffcbcecf2184152971182d772c553448
Leshan Sanjeewa <143873948+leshan99@users.noreply.github.com>
2024-06-07 22:35:51 +0530

    Create main banner

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import './App.css'
 import Navbar from './components/navbar/Navbar'
+import Hero from './components/Hero/Hero'
 
 export const App = () => {
   return (
     <>
       <Navbar/>
+      <div className="container">
+        <Hero/>
+      </div>
     </>
   )
 }
---
+++ portfolio/src/components/Hero/Hero.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Hero.css'
+
+
+const Hero = () => {
+  return (
+    <section className="hero-container">
+        <div className="hero-content">
+            <h2>Transforming Ideas into Digital Realities</h2>
+            <p>DevOps | Cloud Engineer | Frontend Developer</p>
+        </div>
+
+        <div className="hero-img">
+            <div>
+                <div className="tech-icon">
+                    <img width="100px" src="./assets/images/tech1.png" alt=""/>
+                </div>
+                <img className="profile" width="400px" src="./assets/images/Profile.png" alt=""/>
+            </div>
+
+            <div>
+                <div className="tech-icon">
+                    <img width="100px" src="./assets/images/tech2.png" alt=""/>
+                </div>
+                <div className="tech-icon">
+                    <img width="100px" src="./assets/images/tech3.png" alt=""/>
+                </div>
+                <div className="tech-icon">
+                    <img width="100px" src="./assets/images/tech4.png" alt=""/>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
