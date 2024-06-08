704b1202268e7536723e99ec283c4d9cd55cd970
Leshan Sanjeewa <143873948+leshan99@users.noreply.github.com>
2024-06-09 03:33:03 +0530

    Finalized contact form section css and responsive

--- portfolio/src/components/Contact/ContactForm/ContactForm.jsx
+++ portfolio/src/components/Contact/ContactForm/ContactForm.jsx
@@ -3,8 +3,17 @@ import './ContactForm.css'
 
 const ContactForm = () => {
   return (
-    <div>
-      
+    <div className="contact-form-content">
+        <form>
+            <div className="name-container">
+                <input type="text" name="firstname" placeholder="First Name"/>
+                <input type="text" name="lastname" placeholder="Last Name"/>
+            </div>
+            <input type="text" name="email" placeholder="Email"/>
+            <textarea type="text" name="massage" placeholder="Massage" rows={3}/>
+            
+            <button>SEND</button>
+        </form>
     </div>
   )
 }
