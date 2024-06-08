171bc115a4fc76d5e9d3081730eecb950a7817d7
Leshan Sanjeewa <143873948+leshan99@users.noreply.github.com>
2024-06-08 14:48:13 +0530

    Add skill card CSS

--- portfolio/src/components/Skills/Skills.jsx
+++ portfolio/src/components/Skills/Skills.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Skills.css'
+import SkillCard from './SkillCard/SkillCard'
+import { SKILLS } from '../../utils/data'
 
 const Skills = () => {
   return (
@@ -7,7 +9,15 @@ const Skills = () => {
         <h5>Technical Proficiency</h5>
 
         <div className="skills-content">
-            <div className="skills"></div>
+            <div className="skills">
+              {SKILLS.map((item) => (
+                <SkillCard
+                  key={item.title}
+                  iconUrl={item.icon}
+                  title={item.title}
+                />
+              ))}
+            </div>
             <div className="skills-info"></div>
         </div>
     </section>
