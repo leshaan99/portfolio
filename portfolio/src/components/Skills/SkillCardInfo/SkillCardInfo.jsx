01226e05c1e1f515b819a691c63fa5fd30fb7cea
Leshan Sanjeewa <143873948+leshan99@users.noreply.github.com>
2024-06-08 17:07:46 +0530

    Skill card information details show the card

--- portfolio/src/components/Skills/SkillCardInfo/SkillCardInfo.jsx
+++ portfolio/src/components/Skills/SkillCardInfo/SkillCardInfo.jsx
@@ -5,6 +5,21 @@ const SkillCardInfo = ({ heading, skills}) => {
   return (
     <div className="skills-info-card">
         <h6>{heading}</h6>
+
+        <div className="skills-info-content">
+            {skills.map((item, index) => (
+                <React.Fragment key={'skill_${index}'}>
+                    <div className="skill-info">
+                        <p>{item.skill}</p>
+                        <p className="percentage">{item.percentage}</p>
+                    </div>
+
+                    <div className="skill-progress-bg">
+                        <div className="skill-progress" style={{width: item.percentage}} />
+                    </div>
+                </React.Fragment>
+            ))}
+        </div>
     </div>
   )
 }
