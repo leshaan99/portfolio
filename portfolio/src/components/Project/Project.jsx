7eddf569415461a0a99981646854ebfc6c1365ad
Leshan Sanjeewa <143873948+leshan99@users.noreply.github.com>
2024-06-09 00:03:42 +0530

    Create project slider menu

--- portfolio/src/components/Project/Project.jsx
+++ portfolio/src/components/Project/Project.jsx
@@ -1,17 +1,49 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Project.css'
 import ProjectCard from './ProjectCard/ProjectCard'
 import { PROJECT } from '../../utils/data'
+import Slider from "react-slick";
 
 const Project = () => {
+    const sliderRef = useRef();
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrow:false,
+        responsive: [
+            {
+                breakpoint:769,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    };
   return (
     <section className="project-container">
         <h5>Projects</h5>
 
+        <div className="experience-content">
+            <div className="arrow-right">
+                <span class="material-symbols-outlined">chevron_right</span>
+            </div>
+
+            <div className="arrow-left">
+                <span class="material-symbols-outlined">chevron_left</span>
+            </div>
+        </div>
+
         <div className="project-content">
-            {PROJECT.map((item) => (
-                <ProjectCard key={item.title} details={item}/>
+            <Slider ref={sliderRef} {...settings}>
+                {PROJECT.map((item) => (
+                    <ProjectCard key={item.title} details={item}/>
             ))}
+            </Slider>
         </div>
     </section>
   )
--- portfolio/src/components/Project/ProjectCard/ProjectCard.jsx
+++ portfolio/src/components/Project/ProjectCard/ProjectCard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './ProjectCard.css'
 
 const ProjectCard = ({ details }) => {
+
   return (
     <div className="project-card">
         <h6>{details.title}</h6>
