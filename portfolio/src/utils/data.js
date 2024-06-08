6147c1f5784fdaf28aadb2370fdc6ba7edbf2cf4
Leshan Sanjeewa <143873948+leshan99@users.noreply.github.com>
2024-06-08 14:25:13 +0530

    Add skill data

---
+++ portfolio/src/utils/data.js
@@ -0,0 +1,38 @@
+export const SKILLS = [
+    {
+        title: "Frontend",
+        icon: "./assets/images/front.png",
+        skills: [
+            { skill: "HTML5", percentage: "80%"},
+            { skill: "CSS", percentage: "98%"},
+            { skill: "JavaScript", percentage: "70%"},
+            { skill: "ReactJS", percentage: "80%"},
+        ],
+    },
+    {
+        title: "Backend",
+        icon: "./assets/images/back.png",
+        skills: [
+            { skill: "NodeJS", percentage: "70%"},
+            { skill: "Spring Boot", percentage: "60%"},
+        ],
+    },
+    {
+        title: "Tools",
+        icon: "./assets/images/tools.png",
+        skills: [
+            { skill: "Git & GitHub", percentage: "80%"},
+            { skill: "Visual Studio Code", percentage: "80%"},
+            { skill: "Responsive Design", percentage: "60%"},
+        ],
+    },
+    {
+        title: "Soft Skills",
+        icon: "./assets/images/soft.png",
+        skills: [
+            { skill: "Problem Solving", percentage: "80%"},
+            { skill: "Collaboration", percentage: "90%"},
+            { skill: "Team working", percentage: "90%"},
+        ],
+    },
+];
