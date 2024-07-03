ea0b73c5e279bf3e900a0705ea3e2592ba9dc79c
Leshan Sanjeewa <143873948+leshan99@users.noreply.github.com>
2024-07-03 15:16:41 +0530

    Edit footer text

--- portfolio/src/components/Footer/Footer.jsx
+++ portfolio/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className="footer">
-        © Leshan Sanjeewa
+        © 2024 Leshan Sanjeewa. All Rights Reserved.
     </div>
   )
 }
