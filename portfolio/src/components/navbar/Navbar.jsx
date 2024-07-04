17231bda96ceffc9308859ab4bb2779d9197ce24
Leshan Sanjeewa <143873948+leshan99@users.noreply.github.com>
2024-07-04 15:32:59 +0530

    Create new mobile navbar

--- portfolio/src/components/navbar/Navbar.jsx
+++ portfolio/src/components/navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-import React, {} from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { Link } from 'react-scroll';
 
 
+
 const Navbar = () => {
+
+    const [showMenu, setShowMenu] = useState(false)
 
   return (
 
@@ -24,6 +27,7 @@ const Navbar = () => {
                     <li>
                         <Link activeClass='active' to='projects' spy={true} smooth={true} offset={-150} duration={500} className="menu-item">Projects</Link>
                     </li>
+            
 
                     <button className="contact-btn" onClick={() => {
                         document.getElementById('contact').scrollIntoView({behavior: "smooth",block: "nearest"});
@@ -33,6 +37,28 @@ const Navbar = () => {
                     
                 </ul>
 
+            <img className="menulogo" src="./assets/images/menu.png" alt="" onClick={() => setShowMenu(!showMenu)}/>
+
+            </div>
+
+            <div className="mobilenav" style={{display: showMenu? 'flex':'none'}}>
+                <ul>
+                    <li>
+                        <Link activeClass='active' to='home' spy={true} smooth={true} offset={-80} duration={500} className="mobile-menu-item" onClick={() => setShowMenu(false)}>Home</Link>
+                    </li>
+                    <li>
+                        <Link activeClass='active' to='about' spy={true} smooth={true} offset={-140} duration={500} className="mobile-menu-item" onClick={() => setShowMenu(false)}>About</Link>
+                    </li>
+                    <li>
+                        <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-230} duration={500} className="mobile-menu-item" onClick={() => setShowMenu(false)}>Skills</Link>
+                    </li>
+                    <li>
+                        <Link activeClass='active' to='projects' spy={true} smooth={true} offset={-150} duration={500} className="mobile-menu-item" onClick={() => setShowMenu(false)}>Projects</Link>
+                    </li>
+                    <li>
+                        <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-150} duration={500} className="mobile-menu-item" onClick={() => setShowMenu(false)}>Contact</Link>
+                    </li>
+                </ul>
             </div>
         </nav>
   );
